bc783037b120bc514b78c6b13d8198d1e98706fb
rushang99 <46686196+rushang99@users.noreply.github.com>
2019-01-15 00:09:08 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,12 @@ def get(request):
     form1=DateFrom()
     a=request.POST.get('From','')
     b=request.POST.get('To','')
+    if len(a)==0:
+        a='2020-01-01 00:00:00'
+        b='2020-01-01 00:00:00'
+    else:
+        a=a+" 00:00:00"
+        b=b+" 00:00:00"
     posts = Data_set.objects.filter(created_date__gte=a, created_date__lte=b).order_by('created_date')
     #form2=DateTo()
     return render(request, 'blog/post_list.html', {'posts': posts, 'form1':form1})
